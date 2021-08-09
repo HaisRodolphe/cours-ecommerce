7654faf6e561ce127653e437d3bf9790fce07f35
Hais Rodolphe <rodetcel@gmail.com>
2021-08-09 09:00:00 +0200

    master

---
+++ src/Controller/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController
+{
+    public function index()
+    {
+        dd("Ca Fonctionne");
+    }
+
+    public function test(Request $request)
+    {
+
+        $age = $request->query->get('age', 0);
+        // $age = 0;
+
+        // if (!empty($_GET['age'])) {
+        //     $age = $_GET['age'];
+        // };
+
+        return new Response("Vous avez $age ans !");
+    }
+}
