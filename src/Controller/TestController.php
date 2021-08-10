9dc9999c82b5364c6da3759cfce6ea4c495e9047
Hais Rodolphe <rodetcel@gmail.com>
2021-08-10 09:42:58 +0200

    valeurs par défaut et contraintes

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
@@ -11,6 +12,10 @@ class TestController
     {
         dd("Ca Fonctionne");
     }
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name"test", methods={"GET", "POST"}, schemes={"http", "https"})
+     */
 
     public function test(Request $request, $age)
     {
