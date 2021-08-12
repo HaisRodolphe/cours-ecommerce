f92d9a0e9f82ae642f8ca15cb7ccdee9d70d8fd5
Hais Rodolphe <rodetcel@gmail.com>
2021-08-12 07:59:56 +0200

    Update HelloController.php
    Création de container de service de symfony.

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
-
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class HelloController
 {
+    // protected $logger;
+
+    // public function __construct(LoggerInterface $logger)
+    // {
+    //     $this->logger = $logger;
+    // }
     /**
      * @Route("/hello/{prenom?World}", name="name")
      */
 
-    public function Hello($prenom): Response
+    public function Hello($prenom, LoggerInterface $logger): Response
     {
+        $logger->error("Mon message de log !");
+        //$this->logger->error("Mon message de log !");
 
         return new Response("Hello $prenom");
     }
