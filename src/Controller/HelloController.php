c53b077c36d2cfb1213616f8a082a22dac88e1b5
Hais Rodolphe <rodetcel@gmail.com>
2021-08-13 08:22:04 +0200

    Découverte de la notion de container

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,22 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
-    protected $calculator;
-
-    public function __construct(Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-    }
 
     /**
      * @Route("/hello/{prenom?World}", name="name")
      */
 
-    public function Hello($prenom, LoggerInterface $logger): Response
+    public function Hello($prenom, LoggerInterface $logger, Calculator $calculator, Slugify $slugify): Response
     {
+        dump($slugify->slugify("Hello World"));
+
         $logger->error("Mon message de log !");
 
-        $tva = $this->calculator->calcul(100);
+        $tva = $calculator->calcul(100);
 
         dump($tva);
 
